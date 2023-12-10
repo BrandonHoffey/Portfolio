3b7ea12b5eafe9d883f7ed501dec37c9d467dd82
Brandon Hoffey <brandon.hoffey@yahoo.com>
2023-12-10 10:15:30 -0700

    dropdown with list items

--- src/components/main-section/NavigationBar.jsx
+++ src/components/main-section/NavigationBar.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiMenuFries } from "react-icons/ci";
 
 const NavigationBar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const containerStyle = {
+    position: "relative",
+    background: "#023047",
+    height: isDropdownOpen ? "150px" : "50px",
+    transition: "height 0.15s ease",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: "0px",
-    background: "#023047",
-    height: "50px",
+    padding: "0 10px",
   };
+
   const navigationStyle = {
     fontFamily: "'Josefin Slab', sans-serif",
     color: "white",
-    paddingLeft: "10px",
+    margin: "0",
   };
+
+  const dropdownStyle = {
+    display: isDropdownOpen ? "block" : "none",
+    position: "absolute",
+    top: "10px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    opacity: isDropdownOpen ? 1 : 0,
+    transition: "opacity 0.3s ease",
+    padding: "10px",
+  };
+
   const iconStyle = {
     fontSize: "30px",
     color: "white",
@@ -22,13 +39,36 @@ const NavigationBar = () => {
     cursor: "pointer",
   };
 
+  const listStyle = {
+    color: "#fb8500",
+    fontSize: "18px",
+    margin: 0,
+    padding: 12,
+    listStyle: "none",
+    textAlign: "center",
+    width: "150px",
+    transition: "opacity 0.3s ease",
+    fontFamily: "'Josefin Slab', sans-serif",
+  };
+
+  const listItemStyle = {
+    marginBottom: "14px",
+  };
+
   const handleIconClick = () => {
-    console.log("Icon Clicked");
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
     <div style={containerStyle}>
       <h1 style={navigationStyle}>Main Page</h1>
+      <div style={dropdownStyle}>
+        <ul style={listStyle}>
+          <li style={listItemStyle}>-About Me-</li>
+          <li style={listItemStyle}>-My Projects-</li>
+          <li style={listItemStyle}>-Work Experience-</li>
+        </ul>
+      </div>
       <button
         onClick={handleIconClick}
         style={{ background: "none", border: "none" }}
