4e3e5e5286c10b3182a864562dd8f51d50efcbbc
BrandonHoffey <brandon.hoffey@yahoo.com>
2025-01-15 13:21:17 -0700

    Fixed main page image scaling

--- src/components/main-section/MainIndex.jsx
+++ src/components/main-section/MainIndex.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import myImage from "../../assets/aboutMe/myImage.jpg";
 
 const MainIndex = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const containerStyle = {
     background: "#8ecae6",
     height: "690px",
     overflow: "hidden",
-    paddingBottom: "2px"
+    paddingBottom: "2px",
   };
 
   const imageStyle = {
     borderRadius: "60px",
-    maxWidth: "400px",
+    maxWidth: windowWidth <= 768 ? "80%" : "400px",
+    width: "100%",
     display: "block",
     margin: "auto",
-    padding: "40px"
+    padding: windowWidth <= 768 ? "20px" : "40px",
   };
 
   return (
     <div style={containerStyle}>
-      <img style={imageStyle} src={myImage} alt="This is me"></img>
+      <img style={imageStyle} src={myImage} alt="This is me" />
     </div>
   );
 };
